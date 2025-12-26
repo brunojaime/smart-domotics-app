58b94f83b0451079a9cf5df017bd40ddd779b988
bjaime-lg <bruno.jaime@consultants.lgads.tv>
2025-12-25 21:09:43 -0300

    Add access control roles and user model

---
+++ app/src/main/java/com/domotics/smarthome/entities/Permission.kt
@@ -0,0 +1,13 @@
+package com.domotics.smarthome.entities
+
+/**
+ * Enumerates actions that can be granted to roles and users.
+ */
+enum class Permission {
+    VIEW_DEVICES,
+    CONTROL_DEVICES,
+    MANAGE_BUILDINGS,
+    MANAGE_DEVICES,
+    MANAGE_USERS,
+    MANAGE_ROLES
+}
